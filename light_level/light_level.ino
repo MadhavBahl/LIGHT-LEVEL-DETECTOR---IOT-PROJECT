bc4acc06dee07c020a26b02b0b6b5ac0db5a0489
MADHAV BAHL <madhavbahl10@gmail.com>
2017-05-31 10:32:12 +0530

    Add files via upload

---
+++ light_level/light_level.ino
@@ -0,0 +1,21 @@
+int sensorPin = 0;
+int ledPin = 11;
+int ledLevel = 0;
+int inputValue;
+
+void setup() {
+  analogReference(DEFAULT);
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  inputValue = analogRead(sensorPin);
+  inputValue = constrain(inputValue , 100 ,850);
+  ledLevel = map(inputValue,100,800,255,0);
+  delay(450);
+  if(inputValue > 800) ledLevel = 0;
+  Serial.print(ledLevel);
+  Serial.print("\n");
+  analogWrite(ledPin,ledLevel);
+}
